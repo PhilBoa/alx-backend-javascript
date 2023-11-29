fa7c1f7c8c9b5ec8be6850babefd33bb91224e80
PhilBoa <philicbrefo98@gmail.com>
2023-11-29 15:37:16 +0000

    5-photo-reject.js

--- 0x01-ES6_promise/5-photo-reject.js
+++ 0x01-ES6_promise/5-photo-reject.js
@@ -1,9 +1,4 @@
-export default function uploadPhoto(fileName) {
-  return new Promise((resolve, reject) => {
-    // Your processing logic here
-
-    // For demonstration purposes, let's reject the promise with an error message
-    reject(new Error(`${fileName} cannot be processed`));
-  });
+export default function uploadPhoto(filename) {
+  return Promise.reject(new Error(`${filename} cannot be processed`));
 }
 
